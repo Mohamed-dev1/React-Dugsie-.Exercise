d54c7ec8462d54922d272457de1631c140ff5838
Mohamed-dev1 <mohamedsilent8@gmail.com>
2026-08-21 15:55:37 +0300

    Submited Exercise-1

---
+++ ReactJs/Exercise-1/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Usecard from "./Usecard";
+
+function App() {
+  return (
+    <>
+      <Usecard></Usecard>
+    </>
+  );
+}
+
+export default App;
---
+++ ReactJs/Exercise-1/src/Usecard.jsx
@@ -0,0 +1,9 @@
+const Usecard=()=>{
+    return(
+        <>
+        <h1>Mohamed abirkarin</h1>
+        <p>Mohamedsilent801@gmail.com</p>
+        </>
+    )
+}
+export default Usecard
