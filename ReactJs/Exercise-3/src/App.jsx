b10542238714bb4db89fdbd769f79a315554bfe6
Mohamed-dev1 <mohamedsilent8@gmail.com>
2026-08-22 17:42:33 +0300

    Submited Exercise-3

---
+++ ReactJs/Exercise-3/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Greating from "./Greating";
+
+function App() {
+  return (
+    <>
+      <Greating name="mohamed" email="mohamedsilent801@gmail.com" />
+
+      <Greating name="cali" email="cali93@gmail.com" />
+    </>
+  );
+}
+
+export default App;
---
+++ ReactJs/Exercise-3/src/Greating.jsx
@@ -0,0 +1,9 @@
+const Greating = (props) => {
+  return <>
+  <h1>hello, {props.name}</h1>;
+
+  <h1>Emailkeyga waa: , {props.email}</h1>
+  </>
+};
+
+export default Greating;
